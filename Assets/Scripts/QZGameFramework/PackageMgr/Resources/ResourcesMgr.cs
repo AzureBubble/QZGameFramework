13c40d47ba2fd9b6e50f5016bf6437b4caf7e504
Rock <55835505+AzureBubble@users.noreply.github.com>
2024-03-16 00:08:36 +0800

    使用Unitask优化ResourcesMgr中的协程

--- Assets/Scripts/QZGameFramework/PackageMgr/Resources/ResourcesMgr.cs
+++ Assets/Scripts/QZGameFramework/PackageMgr/Resources/ResourcesMgr.cs
@@ -1,19 +1,54 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace QZGameFramework.PackageMgr.ResourcesManager
 {
     public abstract class ResInfoBase
-    { }
+    {
+        /// <summary>
+        /// 引用计数
+        /// </summary>
+        public int RefCount { get; protected set; }
+    }
 
     public class ResInfo<T> : ResInfoBase
     {
+        /// <summary>
+        /// 资源
+        /// </summary>
         public T asset;
+
+        /// <summary>
+        /// 异步加载资源结束后 传递资源给外部的回调函数
+        /// </summary>
         public UnityAction<T> callback;
+
+        public CancellationTokenSource loadCancelTokenSource = new CancellationTokenSource();
+
         public Coroutine coroutine;
-        public bool isDel;
+
+        /// <summary>
+        /// 引用计数为0时 控制资源的释放时机
+        /// </summary>
+        public bool isDelImmediate;
+
+        public void AddRefCount()
+        {
+            ++RefCount;
+        }
+
+        public void SubRefCount()
+        {
+            --RefCount;
+            if (RefCount < 0)
+            {
+                Debug.LogError("The RefCount is less than 0, please check whether resource usage and uninstallation are paired.");
+            }
+        }
     }
 
     public class ResourcesMgr : Singleton<ResourcesMgr>
@@ -26,35 +61,37 @@ namespace QZGameFramework.PackageMgr.ResourcesManager
         /// </summary>
         /// <typeparam name="T">资源类型</typeparam>
         /// <param name="path">资源路径</param>
-        /// <param name="objName">物体实例化后名字</param>
         /// <returns></returns>
         public T LoadRes<T>(string path) where T : Object
         {
             string keyName = typeof(T).Name + "_" + path;
-            ResInfo<T> res;
+            ResInfo<T> resInfo;
             if (resDic.ContainsKey(keyName))
             {
-                res = resDic[keyName] as ResInfo<T>;
-                if (res.asset != null)
+                resInfo = resDic[keyName] as ResInfo<T>;
+                resInfo.AddRefCount();
+                if (resInfo.asset != null)
                 {
-                    return res.asset;
+                    return resInfo.asset;
                 }
                 else
                 {
-                    SingletonManager.StopCoroutine(res.coroutine);
-                    res.asset = Resources.Load<T>(path);
-                    res.callback?.Invoke(res.asset);
-                    res.callback = null;
-                    res.coroutine = null;
-                    return res.asset;
+                    resInfo.loadCancelTokenSource.Cancel();
+                    //SingletonManager.StopCoroutine(resInfo.coroutine);
+                    resInfo.asset = Resources.Load<T>(path);
+                    resInfo.callback?.Invoke(resInfo.asset);
+                    resInfo.callback = null;
+                    resInfo.coroutine = null;
+                    return resInfo.asset;
                 }
             }
             else
             {
-                res = new ResInfo<T>();
-                res.asset = Resources.Load<T>(path);
-                resDic.Add(keyName, res);
-                return res.asset;
+                resInfo = new ResInfo<T>();
+                resInfo.AddRefCount();
+                resInfo.asset = Resources.Load<T>(path);
+                resDic.Add(keyName, resInfo);
+                return resInfo.asset;
             }
         }
 
@@ -64,36 +101,39 @@ namespace QZGameFramework.PackageMgr.ResourcesManager
         /// <typeparam name="T">资源类型</typeparam>
         /// <param name="path">资源路径</param>
         /// <param name="callback">回调函数</param>
-        /// <param name="objName">物体实例化后名字</param>
         public void LoadResAsync<T>(string path, UnityAction<T> callback) where T : UnityEngine.Object
         {
             string keyName = typeof(T).Name + "_" + path;
-            ResInfo<T> res;
+            ResInfo<T> resInfo;
             if (!resDic.ContainsKey(keyName))
             {
                 // 如果没加载过该资源 则开启协程加载
-                res = new ResInfo<T>();
+                resInfo = new ResInfo<T>();
+                // 引用计数
+                resInfo.AddRefCount();
                 // 记录到字典中
-                resDic.Add(keyName, res);
+                resDic.Add(keyName, resInfo);
                 // 记录回调函数
-                res.callback += callback;
-                // 协程异步加载资源
-                res.coroutine = SingletonManager.StartCoroutine(LoadResAsyncCoroutine<T>(path));
+                resInfo.callback += callback;
+                // 异步加载资源
+                //resInfo.coroutine = SingletonManager.StartCoroutine(LoadResAsyncCoroutine<T>(path));
+                LoadResAsyncByUniTask<T>(path, resInfo.loadCancelTokenSource).Forget();
             }
             else
             {
                 // 如果已经开启了加载协程
-                res = resDic[keyName] as ResInfo<T>;
-
-                if (res.asset != null)
+                resInfo = resDic[keyName] as ResInfo<T>;
+                // 引用计数
+                resInfo.AddRefCount();
+                if (resInfo.asset != null)
                 {
                     // 异步加载已经完成 直接调用回调函数
-                    callback?.Invoke(res.asset);
+                    callback?.Invoke(resInfo.asset);
                 }
                 else
                 {
                     // 未完成 则记录回调函数
-                    res.callback += callback;
+                    resInfo.callback += callback;
                 }
             }
         }
@@ -107,19 +147,57 @@ namespace QZGameFramework.PackageMgr.ResourcesManager
             // 根据资源类型的不同进行不同处理
             if (resDic.ContainsKey(keyName))
             {
-                ResInfo<T> res = resDic[keyName] as ResInfo<T>;
-                res.asset = rr.asset as T;
+                ResInfo<T> resInfo = resDic[keyName] as ResInfo<T>;
+                resInfo.asset = rr.asset as T;
 
-                if (res.isDel)
+                if (resInfo.RefCount == 0)
                 {
-                    UnloadAsset<T>(path);
+                    UnloadAsset<T>(path, isSubResCount: false, isDelImmediate: resInfo.isDelImmediate);
                     yield break;
                 }
                 // 等待异步加载完成 进行回调函数
-                res.callback?.Invoke(res.asset);
+                resInfo.callback?.Invoke(resInfo.asset);
                 // 加载完毕 清空回调函数和协程引用
-                res.callback = null;
-                res.coroutine = null;
+                resInfo.callback = null;
+                resInfo.coroutine = null;
+            }
+        }
+
+        /// <summary>
+        /// 使用UniTask实现异步操作
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private async UniTask LoadResAsyncByUniTask<T>(string path, CancellationTokenSource cancellationSource) where T : UnityEngine.Object
+        {
+            string keyName = typeof(T).Name + "_" + path;
+            ResourceRequest rr = Resources.LoadAsync<T>(path);
+            CancellationToken cancellationToken = cancellationSource.Token;
+            // 等待异步加载结束
+            await UniTask.WaitUntil(() => rr.isDone || cancellationToken.IsCancellationRequested);
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            // 根据资源类型的不同进行不同处理
+            if (resDic.ContainsKey(keyName))
+            {
+                ResInfo<T> resInfo = resDic[keyName] as ResInfo<T>;
+                resInfo.asset = rr.asset as T;
+
+                if (resInfo.RefCount == 0)
+                {
+                    UnloadAsset<T>(path, isSubResCount: false, isDelImmediate: resInfo.isDelImmediate);
+                    return;
+                }
+                // 等待异步加载完成 进行回调函数
+                resInfo.callback?.Invoke(resInfo.asset);
+                // 加载完毕 清空回调函数和协程引用
+                resInfo.callback = null;
+                resInfo.coroutine = null;
             }
         }
 
@@ -127,24 +205,81 @@ namespace QZGameFramework.PackageMgr.ResourcesManager
         /// 卸载指定资源
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="path"></param>
-        public void UnloadAsset<T>(string path) where T : Object
+        /// <param name="path">资源路径</param>
+        /// <param name="callback">删除资源后的回调函数</param>
+        /// <param name="isSubResCount">引用计数是否进行减少</param>
+        /// <param name="isDelImmediate">是否立即删除资源</param>
+        public void UnloadAsset<T>(string path, UnityAction<T> callback = null, bool isSubResCount = true, bool isDelImmediate = true) where T : Object
         {
             string keyName = typeof(T).Name + "_" + path;
             if (resDic.ContainsKey(keyName))
             {
-                ResInfo<T> res = resDic[keyName] as ResInfo<T>;
-                if (res.asset != null)
+                ResInfo<T> resInfo = resDic[keyName] as ResInfo<T>;
+                if (isSubResCount)
+                {
+                    resInfo.SubRefCount();
+                }
+                resInfo.isDelImmediate = isDelImmediate;
+                if (resInfo.asset != null && resInfo.RefCount == 0 && isDelImmediate)
                 {
                     resDic.Remove(keyName);
-                    Resources.UnloadAsset(res.asset);
+                    Resources.UnloadAsset(resInfo.asset);
                 }
-                else
+                else if (resInfo.asset == null)
                 {
-                    // 如果资源存在 但还没加载完成 打上删除标记
-                    res.isDel = true;
+                    if (callback != null)
+                    {
+                        resInfo.callback -= callback;
+                    }
                 }
             }
+        }
+
+        /// <summary>
+        /// 异步移除所有不再使用的资源
+        /// </summary>
+        /// <param name="callback"></param>
+        public void UnLoadAllUnusedAssets(UnityAction callback = null)
+        {
+            UnLoadUnusedAssetsAsync(callback).Forget();
+        }
+
+        private async UniTaskVoid UnLoadUnusedAssetsAsync(UnityAction callback)
+        {
+            List<string> removeResInfoList = new List<string>();
+
+            foreach (var resInfoKey in resDic.Keys)
+            {
+                if (resDic[resInfoKey].RefCount == 0)
+                {
+                    removeResInfoList.Add(resInfoKey);
+                }
+            }
+            for (int i = 0; i < removeResInfoList.Count; i++)
+            {
+                resDic.Remove(removeResInfoList[i]);
+            }
+
+            await Resources.UnloadUnusedAssets();
+
+            callback?.Invoke();
+        }
+
+        /// <summary>
+        /// 获取某个资源的引用计数
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public int GetRefCount<T>(string path)
+        {
+            string keyName = typeof(T).Name + "_" + path;
+            if (resDic.TryGetValue(keyName, out ResInfoBase resInfoBase))
+            {
+                return (resInfoBase as ResInfo<T>).RefCount;
+            }
+
+            return 0;
         }
     }
 }
